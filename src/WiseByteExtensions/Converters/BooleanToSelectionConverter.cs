8bfe823d90296670711754c394b032e84fb72df1
Wise Byte Concepts <wisebyteconcepts@gmail.com>
2025-10-25 19:53:07 +0530

    Project Setup

---
+++ src/WiseByteExtensions/Converters/BooleanToSelectionConverter.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.UI.Xaml.Data;
+
+using System;
+
+namespace WiseByteExtensions.Converters;
+
+public class BooleanToSelectionConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
